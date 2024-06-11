612352c7155cb19a98caa296c07338f137d91c90
MrFranSG <131712013+MrFranSG@users.noreply.github.com>
2024-06-11 11:40:01 -0400

    fin parte1
    Añadi el html de la prueba 2, ademas cree las carpetas css donde meti el css de la prueba 2 y cree la carpeta js donde ingrese: credenciales, firebase, java, promesa.
    
    En credenciales ingrese las que me entrego firebase, en firebase y en promesa estoy seguro que me faltaron cosas que añadir pero inicialize las cosas, tambien cree el archivo java pero esta vacio, en el html cree los botones "Aumentar fuente, disminuir fuente, cambiar contraste", tambien cambie el formulario mas especificamente cambie el select anterior que era de año de nacimiento y lo converti en un "Conocer" para indicar el como se conocio de la pagina, el año de nacimiento lo converti en un "date", tambien añadi un checkbox llamado genero que esta sin terminar, un textarea llamado comentario que esta sin terminar y un radio llamado noticias, sin terminar, donde irá algo del tipo "Deseas recibir noticias a tu correo", y por ultimo añadi las redes a un footer para poderlas alinear en el css.

---
+++ js/firebase.js
@@ -0,0 +1,5 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+
+const app = initializeApp(firebaseConfig);
---
+++ js/promesa.js
@@ -0,0 +1,5 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
